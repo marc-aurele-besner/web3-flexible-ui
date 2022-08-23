bdcb01978165b57d56dbe331354add92bbe3fc2b
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-08-23 08:51:41 -0400

    add @mui/material and styles css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 // eslint-disable-next-line 
 declare let window: any
 
+const uiTheme = createTheme();
+
 const App: React.FC = () => {
 
   return (
-    <>
+    <ThemeProvider theme={uiTheme}>
         Hello world!
-    </>
+    </ThemeProvider>
   )
 }
 
