2302c20986d8e61a23006d3ef98691c63e5bd6eb
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-08-23 20:35:22 -0400

    add components

---
+++ src/components/DeployContractsArguments.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { 
+    StyledBody
+ } from '../components/styles'
+import useAbis from '../states/abis'
+
+import contractsList from '../artifacts/contractsList.json'
+
+const StyledButton = styled(Button)`
+    margin: 10px;
+`
+
+const DeployContractsArguments: React.FC = () => {
+    const abis = useAbis(state => state.abis)
+    const setAbis = useAbis(state => state.setAbis)
+
+    useEffect(() => {
+        if (abis === null) {
+            setAbis(contractsList)
+        }
+    }, [abis])
+
+    if (abis !== null)
+    return (
+        <StyledBody>
+            <h2>Deploy contracts</h2>
+            {abis.contractsNames.map((contractName: string) => {
+                return (
+                    
+                    <StyledButton 
+                        variant="contained" 
+                        key={contractName}
+                        // color={parseInt(chainId, 10) === chain.chainId ? "success" : "primary"}
+                        // onClick={async () => {
+                        // await handleSwitchChain(chain.chainId, setChainId, setWallet)
+                        // }}
+                        >
+                            {contractName}
+                        </StyledButton>
+                )})}
+        </StyledBody>
+    )
+}
+
+export default DeployContractsArguments
